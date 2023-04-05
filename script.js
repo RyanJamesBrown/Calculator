f1e1f8dc096cc11e653ff18d41d2a6256a791b2f
RyanJamesBrown <ryanjamesbrown@outlook.com>
2023-04-05 15:52:56 -0500

    working for addition! ' '

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
 let screenDisplay = 0;
+let firstOp = 0;
+let secondOp = 0;
+let operand = '=';
 
-let clearButton = document.getElementById("clearButton");
-clearButton.addEventListener('click',(e)=>{screenDisplay = 0;})
+const clearButton = document.getElementById("clearButton");
+const equalsButton = document.getElementById("equalsButton");
+const gridButtons = document.getElementsByClassName("gridButton");
+const screen = document.getElementById("screen");
+screen.innerHTML = screenDisplay;
 
-let screen = document.getElementById("screen");
+equalsButton.addEventListener('click',(e)=>{
+    if(operand == "+"){
+        secondOp = screenDisplay;
+        screenDisplay = Number(firstOp) + Number(secondOp);
+    }
+    screen.innerHTML = screenDisplay;
+});
+
+clearButton.addEventListener('click',(e)=>{
+    screenDisplay = 0;
+    firstOp = 0;
+    secondOp = 0;
+    screen.innerHTML = screenDisplay;
+});
+
+Array.from(gridButtons).forEach((button)=>
+    button.addEventListener('click',()=>{
+        if(button.textContent == "+"){
+            operand = "+";
+            firstOp = screenDisplay;
+            screenDisplay = '';
+            screen.innerHTML = screenDisplay;
+        }
+        //otherwise if this is a number append to screen
+        else {appendNumber(button.textContent)}
+    })
+);
+
+function appendNumber(number){
+    console.log(number)
+    if(screenDisplay == 0){screenDisplay = ''};
+    screenDisplay += number;
+    screen.innerHTML = screenDisplay;
+}
 screen.innerHTML = screenDisplay;
